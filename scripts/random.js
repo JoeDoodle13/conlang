ee5b833c609dd5d750e1441b6e2560e1ee34891a
Joe <lerchejoseph@gmail.com>
2022-05-10 09:52:50 -0500

    Js reads innerHTML
    I decided to implement a feature: after all the words / definitions,
    there's a section where (soon) you pick how many words you want to be
    randomly selected. Of course, the first step was to get javascript to
    read innerHTML (not the tags, but what's inside, like mama, etc). It'll
    read up to the very last word, and then stop. The goal is to store (push)
    them into an array so that when you hit shuffle, the randomizer will
    select random numbers, which will correspond to words, which it will then
    display.

--- scripts/random.js
+++ scripts/random.js
@@ -1,25 +1,50 @@
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
 
 
 
-function random(maxNum) {
-    return Math.floor(Math.random() * maxNum);
-}
+// function random(maxNum) {
+//     return Math.floor(Math.random() * maxNum);
+// }
 
-let colorArr = [];
-randomColorFunc();
-function randomColorFunc() {
-    for (let i = 0; i < 3; i++) {
-        let x = 255; // rgb
-        let color = random(x);
-        colorArr[colorArr.length] = color;
-    }    
-    return colorArr;
-}
-let color = `rgb(${colorArr})`
-let colorText = `<p style=\"color:${color}\";>${color}</p>`
-document.getElementById("test").innerHTML = colorText;
-console.log(color);
-btn.addEventListener('click',randomColorFunc());
+// let colorArr = [];
+// randomColorFunc();
+// function randomColorFunc() {
+//     for (let i = 0; i < 3; i++) {
+//         let x = 255; // rgb
+//         let color = random(x);
+//         colorArr[colorArr.length] = color;
+//     }    
+//     return colorArr;
+// }
+// let color = `rgb(${colorArr})`
+// let colorText = `<p style=\"color:${color}\";>${color}</p>`
+// document.getElementById("test").innerHTML = colorText;
+// console.log(color);
+// btn.addEventListener('click',randomColorFunc());
 
+
+
+
+
+
+
+
+    const words = document.querySelectorAll(".word");
+    const wordsLength = words.length;
+    console.log(wordsLength);
+    let storeOutput = [];
+    // let output = document.getElementById("shuffle-output").innerHTML;
+
+    // for (let i = 0; i < wordsLength; i++) {
+    //     output = words[i].innerHTML;
+    //     console.log(typeof output)
+    //     console.log(output)
+    //     console.log(i);
+    // }
+
+    for (let i = 0; i < wordsLength; i++) {
+        storeOutput[storeOutput.length] = i;
+        console.log(storeOutput)
+    }
+    console.log(storeOutput)
